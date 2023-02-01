d54a4ba5a1bd5532e64abcc6fa338bf67f18fdff
unknown <sibashishbiswas349@gmail.com>
2023-02-01 08:48:50 +0530

    Setter injection and field injection

---
+++ spring-deomo-annotation/src/com/spring/annotation/RestFullFortuneService.java
@@ -0,0 +1,14 @@
+package com.spring.annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestFullFortuneService implements FortuneService {
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
