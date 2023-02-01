ad6f006e98bfefac9605c5a431b4d06ddd23a208
unknown <sibashishbiswas349@gmail.com>
2023-02-01 10:18:08 +0530

    @Qualifirer annotation in Field injection

--- spring-deomo-annotation/src/com/spring/annotation/TennisCoach.java
+++ spring-deomo-annotation/src/com/spring/annotation/TennisCoach.java
@@ -1,6 +1,7 @@
 package com.spring.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,7 @@ public class TennisCoach implements Coach {
 	
 	//field injection
 	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 
    //Constructor injection
